e46c4ded96d550982469a84fb7bf400460a036cc
sedeco38-gif <sedeco38@gmail.com>
2026-09-09 10:22:27 -0600

    first commit

---
+++ demo_data.js
@@ -0,0 +1,31 @@
+const usuariosDemo = {
+
+    admin: {
+
+        usuario: "admin",
+
+        password: "12345",
+
+        nombre: "Jorge",
+
+        apellidoPaterno: "García",
+
+        apellidoMaterno: "Hernández",
+
+        cargo: "Director de Recursos Materiales",
+
+        correo: "jorge.garcia@hidalgo.gob.mx",
+
+        telefono: "7711234567",
+
+        extension: "204",
+
+        dependencia: "Secretaría de Desarrollo Económico",
+
+        programa: "edificios",
+
+        estatus: "Activo"
+
+    }
+
+};
